ff81620c4a26242cb71cac935ac7c6f986fda555
FernandoYZ <fernandozavala266@gmail.com>
2024-11-03 19:23:11 -0500

    feat(viewmodel): crear IndexViewModel.ts para gestión de datos
    Se crea el archivo IndexViewModel.ts en la carpeta viewmodel, que implementa la lógica para manejar la carga de datos en la aplicación.
    
    Incluye:
    - **Clase IndexViewModel**:
      - Propiedades:
        - model: instancia de IndexModel o
    ull, que almacena los datos estructurados.
        - isLoading: un booleano que indica si los datos están siendo cargados.
        - rror: cadena que almacena mensajes de error, si los hubiera.
    
      - Método loadData():
        - Encargado de cargar los datos mediante la función obtenerDatos del servicio.
        - Establece isLoading en 	rue al iniciar la carga y maneja posibles errores, actualizando el estado de rror en caso de fallo.
        - Al finalizar la carga, establece isLoading en alse.
    
    Este ViewModel proporciona una capa intermedia entre la vista y el modelo, permitiendo una gestión eficiente de los datos y el estado de la aplicación.
    
    Closes 0

---
+++ viewmodel/IndexViewModel.ts
@@ -0,0 +1,27 @@
+import { IndexModel } from '@/models/IndexModel';
+import { obtenerDatos } from '@/services/DataService';
+import { DatosIndex } from '@/services/DataService';
+import IndexData from '../data/index.json'
+
+export class IndexViewModel {
+    model: IndexModel | null = null;
+    isLoading: boolean = false;
+    error: string | null = null;
+
+    async loadData() {
+        this.isLoading = true;
+        this.error = null;
+
+        try {
+            const data = await obtenerDatos(IndexData);
+            // const data = await DatosIndex();
+            this.model = new IndexModel(data);
+        } catch (error) {
+            this.error = 'Error al cargar datos';
+            console.error(error);
+        } finally {
+            this.isLoading = false;
+        }
+    }
+}
+
