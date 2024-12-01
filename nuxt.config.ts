ed3dfb0d517d68ec5b88d80003234117dc83750f
FernandoYZ <fernandozavala266@gmail.com>
2024-12-01 08:25:01 -0500

    chore(config): actualizar configuración para optimización
    1. Cambio de nombre y título de la aplicación
     - El título de la aplicación ahora es "Learnix - Mapa de Conocimiento en Ingeniería" en lugar de "IngenioMap - Mapa de Conocimiento en Ingeniería".
       - Esto afecta a la etiqueta `<title>` de la página y al nombre que se muestra en las pestañas del navegador, mejorando la identidad de la nueva plataforma.
    
    2. Actualización de los metadatos para SEO
    - Se actualizó la descripción de la página en el `meta` tag para reflejar los cambios en la plataforma. Ahora describe "Learnix" como una plataforma de aprendizaje con recursos académicos y cursos interactivos en matemáticas, ciencia y tecnología.
    - Las palabras clave permanecieron en gran parte iguales, pero se ajustaron para reflejar el nuevo nombre y enfoque de la plataforma.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,70 +1,57 @@
 export default defineNuxtConfig({
-  app: {
-    head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
-      title: 'IngenioMap - Mapa de Conocimiento en Ingeniería',
-      meta: [
-        { name: 'description', content: 'IngenioMap es una plataforma de conocimiento en ingeniería diseñada para estudiantes y profesores. Descubre rutas de aprendizaje en diversas áreas de ingeniería para mejorar tus habilidades.' },
-        { name: 'keywords', content: 'ingeniería, mapa de conocimiento, aprendizaje, estudiantes, profesores, educación, tecnología, habilidades en ingeniería' },
-        { name: 'robots', content: 'index, follow' },
-        { property: 'og:title', content: 'IngenioMap - Mapa de Conocimiento en Ingeniería' },
-        { property: 'og:description', content: 'Explora rutas de aprendizaje y guía tu conocimiento en ingeniería con IngenioMap, el recurso ideal para estudiantes y profesores.' },
-        { property: 'og:image', content: 'https://ingeniomap.com/logo.jpg' },
-        { property: 'og:url', content: 'https://ingeniomap.com' },
-        { name: 'twitter:card', content: 'summary_large_image' },
-        { name: 'twitter:title', content: 'IngenioMap - Mapa de Conocimiento en Ingeniería' },
-        { name: 'twitter:description', content: 'Encuentra guías y rutas de aprendizaje en ingeniería con IngenioMap.' },
-        { name: 'twitter:image', content: 'https://ingeniomap.com/logo.jpg' },
-      ],
-      script: [
-        {
-          type: 'application/ld+json',
-          children: JSON.stringify({
-            "@context": "https://schema.org",
-            "@type": "WebSite",
-            "name": "IngenioMap",
-            "url": "https://ingeniomap.com",
-            "author": [
-              {
-                "@type": "Person",
-                "name": "FernandoYZ",
-                "url": "https://github.com/FernandoYZ"
-              },
-              {
-                "@type": "Person",
-                "name": "CarolayDS",
-                "url": "https://github.com/CarolayDS"
-              },
-              {
-                "@type": "Person",
-                "name": "karlaval23",
-                "url": "https://github.com/karlaval23"
-              },
-              {
-                "@type": "Person",
-                "name": "QuerevaluJesus",
-                "url": "https://github.com/QuerevaluJesus"
-              },
-              {
-                "@type": "Person",
-                "name": "fabian18P",
-                "url": "https://github.com/fabian18P"
-              }
+    app: {
+        head: {
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+            title: 'Learnix - Mapa de Conocimiento en Ingeniería',
+            meta: [
+                { name: 'description', content: 'Descubre Learnix: una plataforma de aprendizaje que fusiona recursos académicos con cursos interactivos en matemáticas, ciencia y tecnología. Mejora tus habilidades con contenidos prácticos y desafiantes.' },
+                { name: 'keywords', content: 'ingeniería, mapa de conocimiento, aprendizaje, estudiantes, profesores, educación, tecnología, habilidades en ingeniería' },
+                { name: 'robots', content: 'index, follow' },
+                { property: 'og:title', content: 'Learnix - Mapa de Conocimiento en Ingeniería' },
+                { property: 'og:description', content: 'Explora rutas de aprendizaje y guía tu conocimiento en ingeniería con Learnix, el recurso ideal para estudiantes y profesores.' },
+                { property: 'og:image', content: 'https://Learnix.com/logo.jpg' },
+                { property: 'og:url', content: 'https://Learnix.com' },
+                { name: 'twitter:card', content: 'summary_large_image' },
+                { name: 'twitter:title', content: 'Learnix - Mapa de Conocimiento en Ingeniería' },
+                { name: 'twitter:description', content: 'Encuentra guías y rutas de aprendizaje en ingeniería con Learnix.' },
+                { name: 'twitter:image', content: 'https://Learnix.com/logo.jpg' },
+            ],
+            script: [
+                {
+                    type: 'application/ld+json',
+                    children: JSON.stringify({
+                        "@context": "https://schema.org",
+                        "@type": "WebSite",
+                        "name": "Learnix",
+                        "url": "",
+                        "author": [
+                            {
+                                "@type": "Person",
+                                "name": "FernandoYZ",
+                                "url": "https://github.com/FernandoYZ"
+                            }
+                        ]
+                    })
+                }
+            ],
+            link: [
+                { rel: 'icon', type: 'image/svg+xml', href: '/icon.svg' },
             ]
-          })
         }
-      ],
-      link: [
-        { rel: 'icon', type: 'image/svg+xml', href: '/icon.svg' },
-      ]
+    },
+    compatibilityDate: '2024-04-03',
+    devtools: { enabled: true },
+    modules: ['@nuxtjs/tailwindcss','shadcn-nuxt'],
+    shadcn: {
+        prefix: '',
+        componentDir: './components/ui'
+    },
+    ssr: true,
+    nitro: {
+        preset: 'static',
+    },
+    dir: {
+        public: 'public',
     }
-  },
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
-  shadcn: {
-    prefix: '',
-    componentDir: './components/ui'
-  }
 });
