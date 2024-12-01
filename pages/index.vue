34974c7a951682f8482afebebbe1fbfd1f8ff373
FernandoYZ <fernandozavala266@gmail.com>
2024-12-01 18:36:38 -0500

    feat(pages): reemplazar contenido en index
    - Se reemplazó el contenido estático en la página de inicio por componentes dinámicos.
    - Se agregó el componente `SectionWelcome` para mostrar el título, subtítulo y mensaje de bienvenida de manera dinámica a partir de los datos de `index.json`.
    - Se incluyó el componente `SectionCards` para mostrar las características del mapa de conocimiento.
    - Se añadió el componente `SectionAlcance` para mostrar información de alcance de manera dinámica.
    - Se cargaron los datos desde un archivo JSON (`index.json`) usando `useAsyncData` para poblar las secciones dinámicamente.
    
    #Closes 0

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,23 @@
 <template>
     <div>
-      <Button>Click me</Button>
+        <SectionWelcome
+            :titulo="data.header.title"
+            :subtitulo="data.header.subtitle"
+            :bienvenida="data.welcomeMessage"
+        />
+
+        <SectionCards :data="data" />
+        <SectionAlcance :data="data" />
     </div>
-  </template>
+</template>
+
+<script setup>
+import IndexData from '~/data/index.json'
+
+const { data } = await useAsyncData('index-page', () => {
+    return new Promise((resolve) => {
+        resolve(IndexData)
+    })
+})
+// const data = ref(IndexData)
+</script>
