4dbbbcda235d74b8226b975a1e83f57201e6878e
FernandoYZ <fernandozavala266@gmail.com>
2024-12-13 01:36:37 -0500

    feat(home): crear componente Herramientas.vue
    Se creó el componente `/home/Herramientas.vue`, que contiene una sección con un diseño en dos columnas. La primera columna incluye un título y una descripción centrada en pantallas pequeñas y alineada a la izquierda en pantallas más grandes. El título destaca que el componente ofrece acceso a herramientas de aprendizaje en línea.
    La segunda columna contiene un botón "Ver todas" que lleva al usuario a la página `/herramientas`, donde puede ver todas las herramientas disponibles.
    
    El componente utiliza un fondo degradado, de violeta a negro, con texto blanco, y un botón con colores de fondo en tonos ámbar que cambia al pasar el cursor.
    
    Este componente se añade a la carpeta `home` para seguir la organización modular del proyecto.
    
    #Closes 0

---
+++ components/home/Herramientas.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="flex flex-col md:flex-row items-center justify-between bg-gradient-to-t lg:bg-gradient-to-l from-violet-700 to-black text-white rounded-2xl px-6 md:px-9 py-6 space-y-4 md:space-y-0">
+        <div class="text-center md:text-left">
+            <h2 class="text-xl md:text-2xl font-bold">Descubre las herramientas que te ayudarán a aprender</h2>
+            <p class="text-sm md:text-base mt-2">Acceso a herramientas de aprendizaje en línea</p>
+        </div>
+        <div class="flex justify-center md:justify-end w-full md:w-auto">
+            <a href="/herramientas" aria-label="Ver todas las herramientas">
+                <button 
+                    class="rounded-lg bg-amber-500 px-6 py-2 text-black font-bold hover:bg-amber-400 transition duration-300">
+                    Ver todas
+                </button>
+            </a>
+        </div>
+    </section>
+</template>
