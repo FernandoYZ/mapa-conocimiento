22793695b567377a8af7362f20f78680e5cd7a93
FernandoYZ <fernandozavala266@gmail.com>
2024-12-13 02:07:02 -0500

    feat(pages) rediseñar página Home
    - Se ha actualizado la página `Home.vue` para incluir componentes `HomeHeader`, `HomeHerramientas` y `HomeNodos`.
    - Se ha agregado un contenedor con estilo para mejorar la estructura y diseño de la página.
    
    #Closes 0

--- pages/home.vue
+++ pages/home.vue
@@ -1,3 +1,7 @@
 <template>
-    <div>Página Home</div>
+    <main class="container mx-auto sm:px-6 lg:px-40 py-6 lg:pt-8 lg:pb-16 space-y-6 bg-violet-50">
+        <HomeHeader />
+        <HomeHerramientas />
+        <HomeNodos />
+    </main>
 </template>
