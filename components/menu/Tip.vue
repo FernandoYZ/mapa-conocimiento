7e0a3b8e849e93d8c8d474e3e57a3bb86662bfa5
FernandoYZ <fernandozavala266@gmail.com>
2024-12-13 01:49:43 -0500

    feat(Tip): agregar componente de tooltip
    Se ha creado un nuevo componente `Tip` para mostrar tooltips (mensajes emergentes) cuando el usuario pasa el ratón sobre un elemento.
    
    1. Funcionalidad:
       - El tooltip aparece cuando el usuario coloca el cursor sobre el elemento que envuelve al componente `Tip` (mediante los eventos `mouseenter` y `mouseleave`).
       - El texto del tooltip se pasa como una propiedad `text`, la cual es requerida.
       - El tooltip se posiciona de manera centrada, justo encima del elemento objetivo, con un pequeño triángulo apuntando hacia él.
    
    2. Estilos:
       - El tooltip tiene un fondo oscuro con texto claro, bordes redondeados y un pequeño efecto de sombra.
       - Se incluyen animaciones de entrada y salida para el tooltip, que modifican la opacidad.
    
    Este componente mejora la interacción de la interfaz, proporcionando información contextual al usuario de manera discreta y eficiente.
    
    #Closes 0

---
+++ components/menu/Tip.vue
@@ -0,0 +1,44 @@
+<template>
+    <div 
+        class="relative group"
+        @mouseenter="showTooltip"
+        @mouseleave="hideTooltip"
+    >
+        <slot></slot>
+        <div 
+            v-if="visible" 
+            class="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-4 w-max bg-black text-gray-100 text-xs rounded-md shadow-lg border border-gray-300 py-2 px-4"
+            :style="{ opacity: visible ? 1 : 0 }"
+        >
+            <div class="absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 top-full w-4 h-4 bg-black border-gray-300 rotate-45 shadow-lg"></div>
+            {{ text }}
+        </div>
+    </div>
+</template>
+
+<script setup>  
+const props = defineProps({
+    text: {
+        type: String,
+        required: true
+    }
+});
+
+const visible = ref(false);
+
+const showTooltip = () => visible.value = true;
+const hideTooltip = () => visible.value = false;
+</script>
+  
+<style scoped>
+.tooltip-enter-active,
+.tooltip-leave-active {
+    transition: opacity 0.5s ease-in-out;
+}
+  
+.tooltip-enter,
+.tooltip-leave-to {
+    opacity: 10;
+}
+</style>
+  
