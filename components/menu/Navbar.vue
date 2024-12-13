f05f8e6e7b74cf90c2b5d8de77e03ab1994e9833
FernandoYZ <fernandozavala266@gmail.com>
2024-12-13 01:48:19 -0500

    feat(component): mejorar barra de navegación
    Se han implementado los siguientes cambios en el componente Navbar para mejorar la experiencia de usuario, la accesibilidad y la estructura del código:
    
    1. Accesibilidad:
       - Se ha agregado el atributo `aria-label` en elementos interactivos para mejorar la accesibilidad.
       - Se ha añadido `aria-expanded` y `aria-controls` a los botones de los menús desplegables (Categorías y Herramientas) para indicar su estado (abierto o cerrado) y mejorar la navegación para usuarios de lectores de pantalla.
       - Los menús ahora tienen `id` asignados para mejorar la referencia entre los botones y sus menús asociados.
    
    2. Interacción mejorada:
       - El menú de categorías y herramientas ahora tiene un comportamiento mejorado al cerrar los menús automáticamente si el usuario hace clic fuera de ellos.
       - Se han agregado animaciones de rotación a los iconos de flecha de los botones para indicar el estado de apertura o cierre de los menús.
    
    3. Mejoras visuales:
       - El logo ahora se presenta como una imagen (en lugar de SVG directo), mejorando la claridad visual y la personalización del logo.
       - La barra de navegación ahora está más centrada y ajustada en pantallas grandes mediante el uso de un contenedor `max-w-7xl` y `transform -translate-x-1/2`.
    
    4. Nuevas características:
       - Se han agregado íconos de Modo oscuro y Notificaciones en el lado derecho de la barra de navegación.
       - Se ha optimizado la visibilidad de los menús de "Categorías" y "Herramientas" en dispositivos móviles, utilizando el enfoque de menú desplegable.
    
    Estos cambios mejoran la accesibilidad, la usabilidad y la estética del componente, ofreciendo una experiencia más fluida y profesional para los usuarios.
    
    #Closes 0

--- components/menu/Navbar.vue
+++ components/menu/Navbar.vue
@@ -1,104 +1,111 @@
 <template>
-    <nav class="fixed top-0 left-0 right-0 bg-white shadow-lg z-50">
+    <nav class="fixed top-0 left-0 right-0 bg-white shadow-lg z-50" aria-label="Navegación principal">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="flex justify-between items-center h-16">
-            <!-- Logo - Lado Izquierdo -->
-            <div class="flex-shrink-0 flex items-center">
-                <NuxtLink 
-                    to="/" 
-                    class="flex items-center text-purple-600"
-                >
-                    <div class="flex items-center h-8 w-8 mr-2">
-                        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path opacity="0.4" d="M16.1898 2H7.81976C4.17976 2 2.00977 4.17 2.00977 7.81V16.18C2.00977 19.82 4.17976 21.99 7.81976 21.99H16.1898C19.8298 21.99 21.9998 19.82 21.9998 16.18V7.81C21.9998 4.17 19.8298 2 16.1898 2Z" fill="#6d28d9"/>
-                            <path d="M11.4991 8.09V17.25C11.4991 17.61 11.1391 17.85 10.8091 17.71C9.59914 17.19 8.01913 16.71 6.91913 16.57L6.72913 16.55C6.11913 16.47 5.61914 15.9 5.61914 15.28V7.57999C5.61914 6.81999 6.23915 6.25 6.99915 6.31C8.24915 6.41 10.0992 7.01001 11.2592 7.67001C11.4092 7.74001 11.4991 7.91 11.4991 8.09Z" fill="#6d28d9"/>
-                            <path d="M18.38 7.69983V15.2698C18.38 15.8898 17.88 16.4598 17.27 16.5398L17.06 16.5598C15.97 16.7098 14.4 17.1798 13.19 17.6898C12.86 17.8298 12.5 17.5898 12.5 17.2298V8.0798C12.5 7.8998 12.59 7.72983 12.75 7.63983C13.91 6.98983 15.72 6.4098 16.95 6.2998H16.99C17.76 6.3098 18.38 6.92983 18.38 7.69983Z" fill="#6d28d9"/>
-                        </svg>
-                    </div>
-                    <span class="logo font-bold text-xl">Learnix</span>
-                </NuxtLink>
-            </div>
+                <!-- Logo - Lado Izquierdo -->
+                <div class="flex-shrink-0 flex items-center">
+                    <NuxtLink 
+                        to="/" 
+                        class="flex items-center text-purple-600"
+                        aria-label="Ir a la página principal"
+                    >
+                        <figure class="flex items-center h-8 w-8 mr-2">
+                            <img 
+                                src="/logo.svg" 
+                                alt="Learnix Logo" 
+                                class="h-full w-full"
+                            />
+                        </figure>
+                        <span class="logo font-bold text-xl">Learnix</span>
+                    </NuxtLink>
+                </div>
   
-            <!-- Navigation Links - Centrados -->
-            <div class="absolute left-1/2 transform -translate-x-1/2 flex space-x-8">
-                <NuxtLink 
-                    to="/home" 
-                    class="text-gray-900 inline-flex items-center px-1 pt-1 font-semibold hover:text-purple-700 transition-all duration-100"
-                >
-                Inicio
-                </NuxtLink>
-                
-                <div class="relative categories-toggle">
-                <button
-                    @click="toggleCategories"
-                    class="text-gray-900 inline-flex items-center px-1 pt-1 font-semibold hover:text-purple-700 transition-all duration-100"
-                >
-                    Categorías
-                    <div :class="`ml-1 h-4 w-4 transition-transform duration-200 ${categoriesOpen ? 'rotate-180' : ''}`">
-                        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M6 9L12 15L18 9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>
+                <!-- Navigation Links - Centrados -->
+                <nav class="hidden md:flex space-x-8 absolute left-1/2 transform -translate-x-1/2">
+                    <NuxtLink 
+                        to="/home" 
+                        class="text-gray-900 inline-flex items-center px-1 pt-1 font-semibold hover:text-purple-700 transition-all duration-100"
+                    >
+                        Inicio
+                    </NuxtLink>
+                    
+                    <div class="relative categories-toggle">
+                        <button
+                            @click="toggleCategories"
+                            class="text-gray-900 inline-flex items-center px-1 pt-1 font-semibold hover:text-purple-700 transition-all duration-100"
+                            aria-expanded="categoriesOpen"
+                            aria-controls="categories-menu"
+                        >
+                            Categorías
+                            <span class="ml-1 h-4 w-4 transition-transform duration-200" :class="categoriesOpen ? 'rotate-180' : ''">
+                                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M6 9L12 15L18 9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+                            </span>
+                        </button>
+                        <MenuFull 
+                            :is-visible="categoriesOpen" 
+                            id="categories-menu"
+                            class="absolute left-1/2 transform -translate-x-1/2 w-screen max-w-7xl"
+                        />
                     </div>
-                </button>
-                <MenuFull 
-                    :is-visible="categoriesOpen" 
-                    class="absolute left-0 mt-2"
-                />
-                </div>
-                
-                <NuxtLink 
-                    to="/cursos" 
-                    class="text-gray-900 inline-flex items-center px-1 pt-1 font-semibold hover:text-purple-700 transition-all duration-100"
-                >
-                Cursos
-                </NuxtLink>
-                
-                <div class="relative tools-toggle">
-                <button
-                    @click="toggleTools"
-                    class="text-gray-900 inline-flex items-center px-1 pt-1 font-semibold hover:text-purple-700 transition-all duration-100"
-                >
-                    Herramientas
-                    <div :class="`ml-1 h-4 w-4 transition-transform duration-200 ${toolsOpen ? 'rotate-180' : ''}`">
-                        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M6 9L12 15L18 9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>
+                    
+                    <NuxtLink 
+                        to="/cursos" 
+                        class="text-gray-900 inline-flex items-center px-1 pt-1 font-semibold hover:text-purple-700 transition-all duration-100"
+                    >
+                        Cursos
+                    </NuxtLink>
+                    
+                    <div class="relative tools-toggle">
+                        <button
+                            @click="toggleTools"
+                            class="text-gray-900 inline-flex items-center px-1 pt-1 font-semibold hover:text-purple-700 transition-all duration-100"
+                            aria-expanded="toolsOpen"
+                            aria-controls="tools-menu"
+                        >
+                            Herramientas
+                            <span class="ml-1 h-4 w-4 transition-transform duration-200" :class="toolsOpen ? 'rotate-180' : ''">
+                                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M6 9L12 15L18 9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-lienjoin="round"/>
+                                </svg>
+                            </span>
+                        </button>
+                        <MenuTools
+                            :is-visible="toolsOpen"
+                            id="tools-menu" 
+                            class="absolute right-0"
+                        />
                     </div>
-                </button>
-                <MenuTools
-                    :is-visible="toolsOpen" 
-                    class="absolute right-0 mt-2"
-                />
-                </div>
-            </div>
+                </nav>
   
                 <!-- Right side buttons - Lado Derecho -->
                 <div class="flex items-center space-x-2 text-sm">
-                    <button class="px-4 py-2 bg-purple-600 text-white rounded-md hover:bg-purple-800 transition duration-300">
-                        Iniciar sesión
-                    </button>
-                    <button class="px-4 py-2 border border-purple-600 text-purple-600 rounded-md hover:bg-purple-600 hover:text-white transition duration-300">
-                        Registrarse
-                    </button>
-            
-                    <button 
-                        class="ml-3 p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
-                    >
-                        <div class="h-6 w-6">
-                            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M9.00195 17H5.60636C4.34793 17 3.71872 17 3.58633 16.9023C3.4376 16.7925 3.40126 16.7277 3.38515 16.5436C3.37082 16.3797 3.75646 15.7486 4.52776 14.4866C5.32411 13.1835 6.00031 11.2862 6.00031 8.6C6.00031 7.11479 6.63245 5.69041 7.75766 4.6402C8.88288 3.59 10.409 3 12.0003 3C13.5916 3 15.1177 3.59 16.2429 4.6402C17.3682 5.69041 18.0003 7.11479 18.0003 8.6C18.0003 11.2862 18.6765 13.1835 19.4729 14.4866C20.2441 15.7486 20.6298 16.3797 20.6155 16.5436C20.5994 16.7277 20.563 16.7925 20.4143 16.9023C20.2819 17 19.6527 17 18.3943 17H15.0003M9.00195 17L9.00031 18C9.00031 19.6569 10.3435 21 12.0003 21C13.6572 21 15.0003 19.6569 15.0003 18V17M9.00195 17H15.0003" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <div class="flex space-x-2">
+                        <button 
+                            aria-label="Modo oscuro"
+                            class="p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
+                        >
+                            <svg class="h-7 w-7" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M12 21C16.4183 21 20 17.6439 20 13.504C20 9.76257 17.9654 6.83811 16.562 5.44436C16.3017 5.18584 15.8683 5.30006 15.7212 5.63288C14.9742 7.3229 13.4178 9.75607 11.4286 9.75607C10.1975 9.92086 8.31688 8.86844 9.83483 3.64868C9.97151 3.17868 9.46972 2.80113 9.08645 3.11539C6.9046 4.90436 4 8.51143 4 13.504C4 17.6439 7.58172 21 12 21Z" stroke="currentColor" stroke-width="1.5"/>
                             </svg>
-                        </div>
-                    </button>
+                        </button>
+                        <button 
+                            aria-label="Notificaciones"
+                            class="p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
+                        >
+                            <svg class="h-7 w-7" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M9.00195 17H5.60636C4.34793 17 3.71872 17 3.58633 16.9023C3.4376 16.7925 3.40126 16.7277 3.38515 16.5436C3.37082 16.3797 3.75646 15.7486 4.52776 14.4866C5.32411 13.1835 6.00031 11.2862 6.00031 8.6C6.00031 7.11479 6.63245 5.69041 7.75766 4.6402C8.88288 3.59 10.409 3 12.0003 3C13.5916 3 15.1177 3.59 16.2429 4.6402C17.3682 5.69041 18.0003 7.11479 18.0003 8.6C18.0003 11.2862 18.6765 13.1835 19.4729 14.4866C20.2441 15.7486 20.6298 16.3797 20.6155 16.5436C20.5994 16.7277 20.563 16.7925 20.4143 16.9023C20.2819 17 19.6527 17 18.3943 17H15.0003M9.00195 17L9.00031 18C9.00031 19.6569 10.3435 21 12.0003 21C13.6572 21 15.0003 19.6569 15.0003 18V17M9.00195 17H15.0003" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
     </nav>
 </template>
   
-<script setup>
-import { ref, onMounted, onUnmounted } from 'vue'
-  
+<script setup>  
 const categoriesOpen = ref(false)
 const toolsOpen = ref(false)
   
@@ -116,7 +123,6 @@ const toggleTools = (e) => {
   
 onMounted(() => {
     const closeDropdowns = (e) => {
-        // Check if the click is outside of categories and tools toggles
         if (!e.target.closest('.categories-toggle')) {
             categoriesOpen.value = false
         }
