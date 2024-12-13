1aa9f351c9e59dbb65988839709febc631ce1314
FernandoYZ <fernandozavala266@gmail.com>
2024-12-13 01:41:02 -0500

    feat(index): crear componente Cards.vue
    Se creó el componente `/components/index/Cards.vue` para mostrar las características de un mapa de conocimiento. El componente incluye:
    
    1. Un título centrado que invita a explorar las características del mapa de conocimiento.
    2. Un sistema de carga para mostrar el mensaje "Cargando características..." mientras los datos están siendo obtenidos.
    3. Un diseño en **grid responsivo** que muestra las tarjetas de características en tres columnas en pantallas medianas y se adapta a pantallas pequeñas.
    4. El componente itera sobre los datos de `data.featureCards`, mostrando un componente hijo `CardFeature` para cada característica con su ícono, título y descripción.
    5. Si no hay características disponibles, se muestra un mensaje alternativo: "No hay características disponibles en este momento."
    
    Este componente está diseñado para mostrar las características de manera clara y estructurada, y se ha colocado en la carpeta `index` para mantener la organización del proyecto.
    
    #Closes 0

---
+++ components/index/Cards.vue
@@ -0,0 +1,35 @@
+<template>
+    <section class="container mx-auto px-4 md:px-8 lg:px-16 xl:px-52 py-16 bg-white">
+        <h3 class="text-3xl font-semibold text-center text-tintin-600 mb-10">
+            Explora las características de nuestro mapa de conocimiento
+        </h3>
+        
+        <div v-if="!data" class="text-center">
+            <p>Cargando características...</p>
+        </div>
+    
+        <div v-else>
+            <div v-if="data?.featureCards?.length" class="grid md:grid-cols-3 gap-10">
+            <CardFeature
+                v-for="(card, index) in data.featureCards"
+                :key="index"
+                :icon="card?.icon"
+                :title="card?.title"
+                :description="card?.description"
+            />
+            </div>
+    
+            <p v-else class="text-center">No hay características disponibles en este momento.</p>
+        </div>
+    </section>
+</template>
+  
+<script setup>
+defineProps({
+    data: {
+        type: Object,
+        required: true,
+    }
+});
+</script>
+  
